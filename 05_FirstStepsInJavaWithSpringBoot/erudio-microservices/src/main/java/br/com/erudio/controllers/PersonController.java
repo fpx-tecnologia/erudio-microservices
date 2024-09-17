597144af8e7db6be8492c1d922e2e4120899d234
fpx-tecnologia <fernandocastro.dev@gmail.com>
2024-09-17 17:28:36 -0300

    crud springboot

---
+++ 05_FirstStepsInJavaWithSpringBoot/erudio-microservices/src/main/java/br/com/erudio/controllers/PersonController.java
@@ -0,0 +1,57 @@
+package br.com.erudio.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.Model.Person;
+import br.com.erudio.services.PersonService;
+
+@RestController
+@RequestMapping("/person")
+public class PersonController {
+
+	@Autowired
+	private PersonService personService;
+	
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Person> findAll(){
+		return personService.findAll();
+	}
+	
+	@GetMapping(value = "/{id}", 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person findById(@PathVariable(value="id") Long id) {
+		return personService.findById(id);
+	}
+	
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person create(@RequestBody Person person){
+		return personService.create(person);
+	}
+	
+	@PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Person update(@RequestBody Person person) {
+		return personService.update(person);
+	}
+	
+	@DeleteMapping(value = "/{id}", 
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<?> delete(@PathVariable(name = "id") Long id) {
+		personService.delete(id);
+		return ResponseEntity.noContent().build();
+		
+	}
+}
